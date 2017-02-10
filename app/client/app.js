cda4f6dfd7980bb58e89c3637bf161a978d3c0c1
zarkopernar <zarko.pernar@gmail.com>
2017-02-10 12:31:40 +0100

    Add separate navs, added more service sections

--- app/client/app.js
+++ app/client/app.js
@@ -1,6 +1,7 @@
 import { initWow } from './vanilla/wow'
 
 import 'normalize.css'
+import 'flexboxgrid'
 import './scss/index.scss'
 
 // require('offline-plugin/runtime').install()
--- app/client/vanilla/wow.js
+++ app/client/vanilla/wow.js
@@ -1,14 +1,20 @@
 import AOS from 'aos'
 
+const HEADER_EL = document.getElementById('main-header') 
 
 export function initWow() {
+
+    addScroll()
+    // initCollage()
+
+
     let button = document.getElementById('main-nav-toggle')        
-    let nav = document.getElementById('main-nav')  
-    let header = document.getElementById('main-header')        
+    let nav = document.getElementById('nav-sm')  
+           
     let navActive = false   
     
     button.addEventListener('click', toggleNav)
-    header.addEventListener('click', function(e) {
+    HEADER_EL.addEventListener('click', function(e) {
         e.stopPropagation()
     })
     document.body.addEventListener('click', closeNav)
@@ -34,3 +40,50 @@ export function initWow() {
     AOS.init()
 
 }
+
+
+function addScroll() {
+    let annouce = [...document.querySelectorAll('.annouce')]
+    let wh = document.documentElement.clientHeight
+
+    annouce.forEach((el) => {
+        el.style.transitionDelay = 'none'
+        el.style.transition = 'none'
+    })
+
+    document.addEventListener('scroll', onScroll, {
+        passive: true
+    })
+
+    function onScroll(e) {
+        if (document.documentElement.scrollTop >= wh) {
+            HEADER_EL.classList.add('header--scrolled')
+        } else {
+            HEADER_EL.classList.remove('header--scrolled')
+        }
+
+        let nextPos = document.documentElement.scrollTop / 2
+        annouce.forEach((el) => {
+            requestAnimationFrame(() => {el.style.transform = `translate3d(0, -${nextPos}px, 0)`})
+        })
+    }
+}
+
+// function initCollage() {
+//     let container = document.querySelector('.collage')
+//     let collage = [...document.querySelectorAll('.collage__item')]
+//     let containerWidth
+
+//     window.addEventListener('resize', resize)
+
+//     resize()
+
+//     function resize() {
+//         containerWidth = container.offsetWidth
+//         collage.forEach(resizeItem)
+//     }
+
+//     function resizeItem(el, i) {
+
+//     }
+// }
